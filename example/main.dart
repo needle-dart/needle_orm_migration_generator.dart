b1f3986b95859287703955d10b837904e57054ed
outersky <outersky@gmail.com>
2022-06-03 20:24:39 +0800

    升级到angel3 6.0.0,以支持mariadb数据库

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:angel3_migration_runner/angel3_migration_runner.dart';
+import 'package:angel3_migration_runner/mariadb.dart';
+import 'package:logging/logging.dart';
+import 'package:mysql1/mysql1.dart';
 import 'package:needle_orm/needle_orm.dart';
-import 'package:scope/scope.dart';
 
 import 'src/domain.dart';
 
@@ -13,11 +18,31 @@ class MockDataSource extends DataSource {
     return List<List>.empty();
     // throw UnimplementedError();
   }
+
+  @override
+  Future<T> transaction<T>(FutureOr<T> Function(DataSource p1) f) {
+    throw UnimplementedError();
+  }
 }
 
-void main() {
-  DataSource ds = MockDataSource();
-  (Scope()..value<DataSource>(scopeKeyDefaultDs, ds)).run(test);
-}
+void main(List<String> args) async {
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    print(
+        '${record.level.name}: ${record.time} ${record.loggerName}: ${record.message}');
+  });
+  args = ['refresh'];
+  var settings = new ConnectionSettings(
+      host: 'localhost',
+      port: 3306,
+      user: 'needle',
+      password: 'needle',
+      db: 'needle');
+  var conn = await MySqlConnection.connect(settings);
 
-void test() {}
+  var migrationRunner = MariaDbMigrationRunner(conn, migrations: [
+    BookMigration(),
+    UserMigration(),
+  ]);
+  await runMigrations(migrationRunner, args);
+}
