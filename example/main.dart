38b52eada2e2e4a88d8bd4ddd3f0ff6ace194a5c
outersky <outersky@gmail.com>
2022-06-06 23:19:49 +0800

    small fix

--- example/main.dart
+++ example/main.dart
@@ -1,29 +1,9 @@
-import 'dart:async';
-
 import 'package:angel3_migration_runner/angel3_migration_runner.dart';
 import 'package:angel3_migration_runner/mariadb.dart';
 import 'package:logging/logging.dart';
 import 'package:mysql1/mysql1.dart';
-import 'package:needle_orm/needle_orm.dart';
 
 import 'src/domain.dart';
-
-class MockDataSource extends DataSource {
-  MockDataSource() : super(DatabaseType.PostgreSQL, '10.0');
-  @override
-  Future<List<List>> execute(
-      String tableName, String sql, Map<String, dynamic> substitutionValues,
-      [List<String> returningFields = const []]) async {
-    // print('sql: $sql');
-    return List<List>.empty();
-    // throw UnimplementedError();
-  }
-
-  @override
-  Future<T> transaction<T>(FutureOr<T> Function(DataSource p1) f) {
-    throw UnimplementedError();
-  }
-}
 
 void main(List<String> args) async {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
